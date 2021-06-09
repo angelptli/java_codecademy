cc038a99afd6d87a592cd0a9ccadb88ad029abd1
angelptli <ali95@mail.ccsf.edu>
2021-06-08 21:15:56 -0700

    Set new code template and add new comments

--- java_course/Object-Oriented Java/Learn Java - Methods/Store.java
+++ java_course/Object-Oriented Java/Learn Java - Methods/Store.java
@@ -119,6 +119,74 @@ Instructions
 2. Now, call the advertise() method on the lemonadeStand object two more times.
    It should be called in the main() method three times total.
 ===============================================================================
+Scope
+-----
+A method is a task that an object of a class performs.
+
+We mark the domain of this task using curly braces: {, and }. Everything inside
+the curly braces is part of the task. This domain is called the scope of a
+method.
+
+We can't access variables that are declared inside a method in code that is
+outside the scope of that method.
+
+Looking at the Car class again:
+//////////////////////////////////////////////////////////////
+class Car {
+    String color;
+    int milesDriven;
+
+    public Car(String carColor) {
+        color = carColor;
+        milesDriven = 0;
+    }
+
+    public void drive() {
+        String message = "Miles driven: " + milesDriven;
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Car myFastCar = new Car("red");
+        myFastCar.drive();
+    }
+}
+//////////////////////////////////////////////////////////////
+
+The variable message, which is declared and initialized inside of drive, cannot
+be used inside of main()! It only exists within the scope of the drive()
+method.
+
+However, milesDriven, which is declared at the top of the class, can be used
+inside all methods in the class, since it is in the scope of the whole class.
+
+Instructions
+------------
+1. Inside of the advertise() method, change the productType variable to the
+   cookie variable, which is declared in the main() method. This should also
+   result in the printout:
+   /////////////////////
+   Selling cookies!
+   /////////////////////
+   Right?
+
+2. No! We got an error! The cookie variable cannot be accessed inside of the
+   advertise method. The scope is wrong! Change it back to productType:
+   /////////////////////////////////////////////////////
+   String message = "Selling " + productType + "!";
+   /////////////////////////////////////////////////////
+
+3. Inside of the main() method, print the String message, which is declared in
+   the advertise() method. This should print:
+   /////////////////////
+   Selling Cookies!
+   /////////////////////
+   Right?
+
+4. Foiled again! The message variable only exists inside the scope of the
+   advertise() method! Delete the faulty print statement from the main()
+   method.
+===============================================================================
 */
 public class Store {
     // instance fields
@@ -131,17 +199,23 @@ public class Store {
 
     // advertise method
     public void advertise() {
-        System.out.println("Come spend some money!");
+        // System.out.println("Come spend some money!");
         System.out.println("Selling " + productType + " ! ");
+        System.out.println(message);
     }
 
     // main method
     public static void main(String[] args) {
-        Store lemonadeStand = new Store("Lemonade");
-        // Call the advertise() method on the lemonadeStand object
-        lemonadeStand.advertise();
-        lemonadeStand.advertise();
-        lemonadeStand.advertise();
-        lemonadeStand.advertise();
+        // Store lemonadeStand = new Store("Lemonade");
+        // // Call the advertise() method on the lemonadeStand object
+        // lemonadeStand.advertise();
+        // lemonadeStand.advertise();
+        // lemonadeStand.advertise();
+        // lemonadeStand.advertise();
+
+        String cookie = "Cookies";
+        Store cookieShop = new Store(cookie);
+
+        cookieShop.advertise();
     }
 }
