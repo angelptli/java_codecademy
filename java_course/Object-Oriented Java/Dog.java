8a3cf7946894ea398c3a3ab6d70942059e575c01
angelptli <ali95@mail.ccsf.edu>
2021-06-08 19:08:48 -0700

    Add class Dog code

--- java_course/Object-Oriented Java/Dog.java
+++ java_course/Object-Oriented Java/Dog.java
@@ -44,5 +44,36 @@ Access and print different fields.
 ===============================================================================
 */
 public class Dog {
-    
+    String breed;
+    boolean hasOwner;
+    int age;
+    double weight;
+
+    public Dog(String dogBreed, boolean dogOwned, int dogYears,
+               double dogPounds) {
+        System.out.println("Constructor invoked...");
+        breed = dogBreed;
+        hasOwner = dogOwned;
+        age = dogYears;
+        weight = dogPounds;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main method started...");
+
+        Dog fido = new Dog("poodle", false, 4, 23.4);
+        Dog nunzio = new Dog("shiba inu", true, 12, 35.2);
+        boolean isFidoOlder = fido.age > nunzio.age;
+        int totalDogYears = nunzio.age + fido.age;
+        boolean isNunzioChonkier = nunzio.weight > fido.weight;
+
+        System.out.println("Two doggos recorded: " + fido.breed + "and a"
+                + nunzio.breed);
+        System.out.println("The statement that fido is an older doggo is: "
+                + isFidoOlder);
+        System.out.println("The total age of the doggos is: " + totalDogYears);
+        System.out.println("The statement that nunzio is a chonkier doggo is: "
+                + isNunzioChonkier);
+        System.out.println("Main method finished.");
+    }
 }
