b34dcf783e36e2d38763a39c9c5244972221f78d
angelptli <ali95@mail.ccsf.edu>
2021-06-18 00:31:50 -0700

    Add code template

--- java_course/Arrays and ArrayLists/Learn Java - Arrays/Newsfeed3.java
+++ java_course/Arrays and ArrayLists/Learn Java - Arrays/Newsfeed3.java
@@ -73,6 +73,32 @@ Instructions
    the value of favoriteArticles at index 2 would be set to "Celebrity Hands
    Throughout the Decades".
 */
+import java.util.Arrays;
+
 public class Newsfeed3 {
     
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    int[] views = {0, 0, 0, 0};
+    String[] favoriteArticles;
+    
+    public Newsfeed() {
+        // Initialize favoriteArticles here:
+      
+    }
+    
+    public void setFavoriteArticle(int favoriteIndex, String newArticle) {
+        // Add newArticle to favoriteArticles:
+      
+      
+    }
+      
+    public static void main(String[] args) {
+        Newsfeed sampleFeed = new Newsfeed();
+        
+        sampleFeed.setFavoriteArticle(2, "Humans: Exterminate Or Not?");
+        sampleFeed.setFavoriteArticle(3, "Organic Eye Implants");
+        sampleFeed.setFavoriteArticle(0, "Oil News");
+        
+        System.out.println(Arrays.toString(sampleFeed.favoriteArticles));
+    }
 }
