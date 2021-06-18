ae81e5fdce457bdd9a9772b429c960ff864d0fed
angelptli <ali95@mail.ccsf.edu>
2021-06-17 20:42:51 -0700

    Complete Conditional-And: && exercise

--- java_course/Conditional and Control Flow/Conditional Operators/Reservation.java
+++ java_course/Conditional and Control Flow/Conditional Operators/Reservation.java
@@ -80,18 +80,25 @@ public class Reservation {
                 print "Reservation denied"
                 set isConfirmed to false
         */
+        if (restaurantCapacity >= guestCount && isRestaurantOpen) {
+            System.out.println("Reservation confirmed");
+            isConfirmed = true;
+        } else {
+            System.out.println("Reservation denied");
+            isConfirmed = false;
+        }
     }
   
-      public void informUser() {
-          System.out.println("Please enjoy your meal!");
-      }
+    public void informUser() {
+        System.out.println("Please enjoy your meal!");
+    }
   
-      public static void main(String[] args) {
-          Reservation partyOfThree = new Reservation(3, 12, true);
-          Reservation partyOfFour = new Reservation(4, 3, true);
-          partyOfThree.confirmReservation();
-          partyOfThree.informUser();
-          partyOfFour.confirmReservation();
-          partyOfFour.informUser();
+    public static void main(String[] args) {
+        Reservation partyOfThree = new Reservation(3, 12, true);
+        Reservation partyOfFour = new Reservation(4, 3, true);
+        partyOfThree.confirmReservation();
+        partyOfThree.informUser();
+        partyOfFour.confirmReservation();
+        partyOfFour.informUser();
     }
 }
