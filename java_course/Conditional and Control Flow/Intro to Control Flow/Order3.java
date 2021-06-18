2230ba2b51948d270b87858040b70b2a302be8ab
angelptli <ali95@mail.ccsf.edu>
2021-06-17 19:47:42 -0700

    Construct and complete Nested Conditional Statements exercise

--- java_course/Conditional and Control Flow/Intro to Control Flow/Order3.java
+++ java_course/Conditional and Control Flow/Intro to Control Flow/Order3.java
@@ -1,3 +1,106 @@
+/*
+================================================================================
+Nested Conditional Statements
+-----------------------------
+We can create more complex conditional structures by creating nested conditional
+statements, which is created by placing condititonal statements inside other
+conditional statements:
+////////////////////////////////////////////////////////////////
+if (outer condition) {
+    if (nested condition) {
+        Instruction to execute if both conditions are true
+    }
+}
+////////////////////////////////////////////////////////////////
+
+When we implement nested conditional statements, the outer statement is
+evaluated first. If the outer condition is true, then the inner, nested
+statement is evaluated.
+
+Let's create a program that helps us decide what to wear based on the weather:
+/////////////////////////////////////////////////////////////
+int temp = 45;
+boolean raining = true;
+
+if (temp < 60) {
+    System.out.println("Wear a jacket!");
+    if (raining == true) {
+        System.out.println("Bbring your umbrella.";
+    } else {
+        System.out.println("Leave your umbrella home.");
+    }
+}
+/////////////////////////////////////////////////////////////
+
+In the code snippet above, our compiler will check the condition in the first
+if-then statement: temp < 60. Since temp has a value of 45, this condition is
+true; therefore, our program will print "Wear a jacket!"
+
+Then, we'll evaluate the condition of the nested if-then statement: raining ==
+true. This condition is also true, so "Bring your umbrella" is also printed to
+the screen.
+
+Note that, if the first condition was false, the nested condition would not be
+evaluated.
+
+Instructions
+------------
+The company offers a temporary deal that, if the consumer uses the coupon
+"ship50", the company will reduce the express shipping price. Let's rewrite the
+body of else-if statement from the last exercise. Inside the else-if statement,
+create a nested if-then statement that checks if couponCode equals "ship50". If
+the nested condition is true, return the value .85. If the condition is false,
+use a nested else statement to return the value 1.75.
+================================================================================
+*/
 public class Order3 {
-    
+    boolean isFilled;
+    double billAmount;
+    String shipping;
+    String couponCode;
+
+    public Order3(boolean filled, double cost, String shippingMethod,
+                  String coupon) {
+        if (cost > 24.00) {
+            System.out.println("High value item!");
+        }
+        isFilled = filled;
+        billAmount = cost;
+        shipping = shippingMethod;
+        couponCode = coupon;
+    }
+
+    public void ship() {
+        if (isFilled) {
+            System.out.println("Shipping");
+            System.out.println("Shipping cost: " + calculateShipping());
+        } else {
+            System.out.println("Order not ready");
+        }
+    }
+
+    public double calculateShipping() {
+        if (shipping.equals("Regular")) {
+            return 0;
+        } else if (shipping.equals("Express")) {
+            // Add your code here
+            if (couponCode.equals("ship50")) {
+                return .85;
+            } else {
+                return 1.75;
+            }
+            
+        } else {
+            return .50;
+        }
+    }
+
+    public static void main(String[] args) {
+        // do not alter the main method!
+        Order3 book = new Order3(true, 9.99, "Express", "ship50");
+        Order3 chemistrySet = new Order3(false, 72.50, "Regular",
+                                        "freeShipping");
+        book.ship();
+        chemistrySet.ship();
+    }
 }
