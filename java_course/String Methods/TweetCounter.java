e8f6053289b0b21afbbe3a7163f3787e172388e5
angelptli <ali95@mail.ccsf.edu>
2021-06-28 14:47:38 -0700

    Fix indentation

--- java_course/String Methods/TweetCounter.java
+++ java_course/String Methods/TweetCounter.java
@@ -22,15 +22,15 @@ Instructions
 */
 public class TweetCounter {
   
-	public static void main(String[] args) {
+    public static void main(String[] args) {
     
-    String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know"
-                   + "any personally. Get on that.";  
-
-    // What's the character count?
-    System.out.println(tweet.length());
+        String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know"
+                       + "any personally. Get on that.";  
+        
+        // What's the character count?
+        System.out.println(tweet.length());
     
     
-  }
+    }
   
 }
