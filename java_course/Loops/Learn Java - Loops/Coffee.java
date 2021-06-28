cc6fd7c0325c28fa277d91908c503733ea2a6c89
angelptli <ali95@mail.ccsf.edu>
2021-06-27 20:18:45 -0700

    Complete Incrementing While Loops exercise

--- java_course/Loops/Learn Java - Loops/Coffee.java
+++ java_course/Loops/Learn Java - Loops/Coffee.java
@@ -72,9 +72,16 @@ Instructions
 class Coffee {
 
     public static void main(String[] args) {
-        
+
         // initialize cupsOfCoffee
+        int cupsOfCoffee = 1;
 
         // add while loop with counter
+        while (cupsOfCoffee <= 100) {
+            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+            cupsOfCoffee++;
+        }
+
+
     }
 }
