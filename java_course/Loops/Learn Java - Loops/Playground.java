2bda1a72abd85db813271b676843c27b1fc7182c
angelptli <ali95@mail.ccsf.edu>
2021-06-27 22:23:10 -0700

    Construct Review exercises

---
+++ java_course/Loops/Learn Java - Loops/Playground.java
@@ -0,0 +1,50 @@
+/*
+================================================================================
+Review
+------
+Nice work! Let's iterate over what you've just learned about loops:
+
+- while loops: These are useful to repeat a code block an unknown number of
+  times until some condition is met. For example:
+  /////////////////////////
+  int wishes = 0;
+
+  while (wishes < 3) {
+      wishes++
+  }
+  /////////////////////////
+
+- for loops: Theses are ideal for when you are incrementing or decrementing
+  with a counter variable. For example:
+  //////////////////////////////////
+  for (int i = o; i < 5; i++) {
+      // code that will run
+  }
+  //////////////////////////////////
+
+- For-each loops: These make it simple to do something with each item in a
+  list. For example:
+  //////////////////////////////////////////////////
+  for (String inventoryItem : inventoryItems) {
+      // do something with each inventoryItem
+  }
+  //////////////////////////////////////////////////
+
+Instructions
+------------
+Feel free to play around with loops, arrays, and ArrayLists in the code editor.
+================================================================================
+*/
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Playground {
+
+	public static void main(String[] args) {
+  
+    for (int i = 0; i < 28; i++) {
+  		System.out.println("Congrats on finishing Java loops!");
+	  }
+  }
+
+}
