9791ddfcde14e45c0779997f8d16ae2463883886
angelptli <ali95@mail.ccsf.edu>
2021-06-28 13:47:04 -0700

    Complete concat() exercise

--- java_course/String Methods/CombineNames.java
+++ java_course/String Methods/CombineNames.java
@@ -62,11 +62,11 @@ public class CombineNames {
     public static void main(String[] args) {
     
         // Add your first names:
-        String firstName = "";  
-        String lastName = "";
+        String firstName = "Angel ";  
+        String lastName = "Li";
 
         // What's the full name?
-    
+        System.out.println(firstName.concat(lastName));
     
     }
   
